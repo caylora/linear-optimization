Fixed error where infeasible problem produced out of bounds error, and incorrect results.

src/linearoptimization/__main__.py
@@ -4,20 +4,27 @@ import linearoptimization as lo
 
 import numpy as np
 
-variables, contraints = 3, 3  # Number of variables, constraints
-A = np.zeros(
-    (contraints, variables), np.double
-)  # Matrix of coefficients of constraints
-b = np.zeros(contraints, np.double)  # Vector of constraint values
-c = np.zeros(variables, np.double)  # Vector of objective function variables
+MAX_N, MAX_M = 1001, 1001
+A = np.empty((MAX_M, MAX_N))  # Matrix of coefficients of constraints
+b = np.empty(MAX_M)  # Vector of constraint values
+c = np.empty(MAX_N, np.double)  # Vector of objective function variables
 v = 0  # Constant
+N = np.empty(MAX_N, np.intc)  # Nonbasic variables
+B = np.empty(MAX_M, np.intc)  # Basic variables
 
 # Define the parameters of the problem:
-A[0, 0], A[0, 1], A[0, 2] = -2, -3, -1
-A[1, 0], A[1, 1], A[1, 2] = -4, -1, -2
-A[2, 0], A[2, 1], A[2, 2] = -3, -4, -2
-b[0], b[1], b[2] = 5, 11, 8
-c[0], c[1], c[2] = 5, 4, 3
+variables, contraints = 2, 2  # Number of variables, constraints
+# A[0, 0], A[0, 1], A[0, 2] = -2, -3, -1
+# A[1, 0], A[1, 1], A[1, 2] = -4, -1, -2
+# A[2, 0], A[2, 1], A[2, 2] = -3, -4, -2
+# b[0], b[1], b[2] = 5, 11, 8
+# c[0], c[1], c[2] = 5, 4, 3
+
+# infeasible example
+A[0][0], A[0][1] = -1, -1
+A[1][0], A[1][1] = 2, 2
+b[0], b[1] = 2, -10
+c[0], c[1] = 3, -2
 
 # Pass the parameters to the solving algorithm
-lo.run(variables, contraints, A, b, c, v)
+lo.run(variables, contraints, A, b, c, v, N, B)

src/linearoptimization/linearoptimization.py
@@ -41,7 +41,6 @@ def iterate_simplex():
     print("--------------------")
     print("State:")
     print("Maximize: ", end="")
-    global n
     for j in range(n):
         print(f"{c[j]}x_{N[j]} + ", end="")
     print(f"{v}")
@@ -100,7 +99,7 @@ def initialize_simplex():
     v_old = v
     # Aux. objective function
     c[n - 1] = -1
-    for j in range(n):
+    for j in range(n - 1):
         c[j] = 0
     v = 0
     # Aux. coefficients
@@ -188,6 +187,8 @@ def run(
     constr_value_vector,
     objfunc_coeff_vector,
     constant,
+    nonbasic_vector,
+    basic_vector,
 ):
     """Main entrypoint for the code."""
     global n, m, A, b, c, v, N, B
@@ -197,8 +198,8 @@ def run(
     b = constr_value_vector  # Vector of constraint values
     c = objfunc_coeff_vector  # Vector of objective function variables
     v = constant  # Constant
-    N = np.zeros(n, np.intc)  # Nonbasic variables
-    B = np.zeros(m, np.intc)  # Basic variables
+    N = nonbasic_vector  # Nonbasic variables
+    B = basic_vector  # Basic variables
 
     ret = simplex()
     if ret[1] == np.inf: