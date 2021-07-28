809b79c6ff90448fd690ae7afba19f64cd50611c
Alton Caylor <caylora@allegheny.edu>
2021-07-28 13:36:10 -0400

    Revision for battery component.

--- src/linear_optimization/__main__.py
+++ src/linear_optimization/__main__.py
@@ -7,6 +7,7 @@ import numpy as np
 MAX_N, MAX_M = 10001, 10001
 
 ARRAY_COST = 3000  # $/kW
+TAX_MODIFIER = 0.74
 BATTERY_COST = 625  # $/kWh
 P_RETAIL_COST = si.calculate_future_power_costs(
     0.134,  # current $/kWh
@@ -16,11 +17,17 @@ P_RETAIL_COST = si.calculate_future_power_costs(
 P_WHOLESALE_COST = 0.0646  # $/kWh
 ROOF_AREA = 30
 AREA_USAGE = 5.181  # m^2/kW
-_, ANNUAL_PRODUCTION = si.read_pvwatts("input/pvwatts_hourly.csv")  # kWh/yr
+PRODUCTION_DATA, ANNUAL_PRODUCTION = si.read_pvwatts(
+    "input/pvwatts_hourly.csv"
+)  # kWh/yr
 BATTERY_STORAGE_TARGET = 24  # hrs
 USAGE_DATA, TOTAL_USAGE = si.read_usage("input/usage.csv")
 USAGE_CONSTRAINTS = si.generate_constraints(
     USAGE_DATA,
+    BATTERY_STORAGE_TARGET,
+)
+PRODUCTION_CONSTRAINTS = si.generate_constraints(
+    PRODUCTION_DATA,
     BATTERY_STORAGE_TARGET,
 )
 
@@ -38,17 +45,17 @@ A[0, 0], A[0, 1], A[0, 2] = -ANNUAL_PRODUCTION, 0, 0
 A[1, 0], A[1, 1], A[1, 2] = -AREA_USAGE, -AREA_USAGE, 0
 b[0], b[1] = TOTAL_USAGE, ROOF_AREA
 c[0], c[1], c[2] = (
-    (-ARRAY_COST + P_RETAIL_COST * ANNUAL_PRODUCTION),
-    (-ARRAY_COST + P_WHOLESALE_COST * ANNUAL_PRODUCTION),
+    (-0.74 * ARRAY_COST + P_RETAIL_COST * ANNUAL_PRODUCTION),
+    (-0.74 * ARRAY_COST + P_WHOLESALE_COST * ANNUAL_PRODUCTION),
     -BATTERY_COST,
 )
-
-# A[2,0], A[2,1], A[2,2] = 0, 0, 1
-# b[2] = max(USAGE_CONSTRAINTS)
-for i in range(len(USAGE_CONSTRAINTS)):
-    A[i + 2, 0], A[i + 2, 1], A[i + 2, 2] = 0, 0, 1
-    b[i + 2] = -USAGE_CONSTRAINTS[i]
-variables, constraints = 3, len(USAGE_CONSTRAINTS) + 2
+loc = np.argmax(USAGE_CONSTRAINTS)
+A[2, 0], A[2, 1], A[2, 2] = PRODUCTION_CONSTRAINTS[loc], PRODUCTION_CONSTRAINTS[loc], 1
+b[2] = -max(USAGE_CONSTRAINTS)
+# for i in range(len(USAGE_CONSTRAINTS)):
+#     A[i + 2, 0], A[i + 2, 1], A[i + 2, 2] = PRODUCTION_CONSTRAINTS[i], PRODUCTION_CONSTRAINTS[i], 1
+#     b[i + 2] = -USAGE_CONSTRAINTS[i]
+variables, constraints = 3, 3
 
 # C_A = cost of array           $/kW
 # x_A1 = solar cap. retail      kW
